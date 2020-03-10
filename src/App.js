bda8c01efb22a80984bdf497f8c8926606a20db4
sparksys98 <hanineiroukh@gmail.com>
2020-03-10 15:14:52 +0200

    meh

--- src/App.js
+++ src/App.js
@@ -21,39 +21,39 @@ function App() {
           <div class="col-lg-4 col-md-6 col-12">
             <div class="card">
               <div class="image">
-                <img class="card-img-top img-fluid" src="https://nation.com.pk/digital_images/large/2015-05-10/jk-rowling-believes-in-standing-up-to-bullies-1431261222-3596.jpeg" alt="J.K. Rowling"/>
+                <img class="card-img-top img-fluid" src={authors[0].imageUrl} alt="J.K. Rowling"/>
               </div>
               <div class="card-body">
                 <h5 class="card-title">
                   <span>{`${authors[0].first_name} ${authors[0].last_name}`}</span>
                 </h5>
-                <small class="card-text">7 books</small>
+                <small class="card-text">{authors[0].books.length} books</small>
               </div>
             </div>
           </div>
           <div class="col-lg-4 col-md-6 col-12">
             <div class="card">
               <div class="image">
-                <img class="card-img-top img-fluid" src="https://typeset-beta.imgix.net/elite-daily/2016/03/06144207/elite-daily-George-RR-Martin.jpg?w=800&h=800&auto=format&q=70&fit=crop&crop=faces" alt="George R.R. Martin"/>
+                <img class="card-img-top img-fluid" src={authors[1].imageUrl} alt="George R.R. Martin"/>
               </div>
               <div class="card-body">
                 <h5 class="card-title">
                   <span>{`${authors[1].first_name} ${authors[1].last_name}`}</span>
                 </h5>
-                <small class="card-text">5 books</small>
+                <small class="card-text">{authors[1].books.length} books</small>
               </div>
             </div>
           </div>
           <div class="col-lg-4 col-md-6 col-12">
             <div class="card">
               <div class="image">
-                <img class="card-img-top img-fluid" src="https://images.8tracks.com/cover/i/010/010/920/tolkeinbigger-2362.jpg?rect=166,0,500,500&q=98&fm=jpg&fit=max" alt="J.R.R Tolkein"/>
+                <img class="card-img-top img-fluid" src={authors[2].imageUrl} alt="J.R.R Tolkein"/>
               </div>
               <div class="card-body">
                 <h5 class="card-title">
                   <span>{`${authors[2].first_name} ${authors[2].last_name}`}</span>
                 </h5>
-                <small class="card-text">6 books</small>
+                <small class="card-text">{authors[2].books.length} books</small>
               </div>
             </div>
           </div>
